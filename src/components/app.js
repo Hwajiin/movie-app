1d3471bae08f5d8c7e4f085d3b3eaa1fdbcae148
Hwajiin <borderline0px@gmail.com>
2021-09-10 15:18:37 +0900

    project setting

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GlobalStyles from "./globalStyles";
+
+const App = () => {
+  return (
+    <div className="App">
+      <GlobalStyles />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/router.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Router = (props) => <h1>Router</h1>;
+
+export default Router;
