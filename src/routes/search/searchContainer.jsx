25dddb44c0bb3beba2a901a0d02e4d7a975fb5c7
Hwajiin <borderline0px@gmail.com>
2021-09-13 13:55:17 +0900

    make searchContainer & searchPresenter

--- src/routes/search/searchContainer.jsx
+++ src/routes/search/searchContainer.jsx
@@ -1,16 +1,63 @@
-import React from "react";
-import { useState } from "react";
-import { useHistory } from "react-router";
+import React, { useEffect, useCallback, useState } from "react";
 import SearchPresenter from "./searchPresenter";
 
 const SearchContainer = ({ api }) => {
   const initialState = {
-    isLoading: true,
+    moviesResults: null,
+    tvResults: null,
   };
 
   const [data, setData] = useState(initialState);
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  return <SearchPresenter />;
+  const onClear = () => {
+    setText("");
+  };
+
+  const onSubmit = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    loadData();
+    setLoading(false);
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setText(value);
+  };
+
+  const loadData = useCallback(async () => {
+    if (text !== "") {
+      const {
+        data: { results: moviesResults },
+      } = await api.search.movie(text);
+      const {
+        data: { results: tvResults },
+      } = await api.search.tv(text);
+      setData({
+        moviesResults,
+        tvResults,
+      });
+    }
+  }, [api, text]);
+
+  useEffect(() => {
+    loadData();
+  }, [api, loadData]);
+
+  return (
+    <SearchPresenter
+      data={data}
+      onSubmit={onSubmit}
+      isLoading={loading}
+      onClear={onClear}
+      onChange={onChange}
+      searchTerm={text}
+    />
+  );
 };
 
 export default SearchContainer;
--- src/routes/search/searchPresenter.jsx
+++ src/routes/search/searchPresenter.jsx
@@ -1,5 +1,111 @@
 import React from "react";
+import styled from "styled-components";
+import Loader from "../../components/loader";
+import Poster from "../../components/poster";
+import Section from "../../components/section";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const SearchPresenter = (props) => <h1>Search</h1>;
+const Container = styled.div`
+  width: 100%;
+  padding: 20px;
+`;
+
+const Form = styled.form`
+  position: relative;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 3px;
+  margin-bottom: 30px;
+`;
+
+const Input = styled.input`
+  all: unset;
+  width: 100%;
+  padding: 7px 10px;
+  font-size: 13px;
+`;
+
+const Button = styled.button`
+  all: unset;
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  width: 18px;
+  height: 18px;
+  background-color: var(--grey-color);
+  border-radius: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+  font-size: 13px;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.7);
+`;
+
+const SearchPresenter = ({
+  onSubmit,
+  onChange,
+  isLoading,
+  onClear,
+  searchTerm,
+  data: { moviesResults, tvResults },
+}) => {
+  return (
+    <Container>
+      <Form onSubmit={onSubmit}>
+        <Input
+          type="text"
+          placeholder="검색어를 입력해주세요"
+          value={searchTerm}
+          onChange={onChange}
+        />
+        <Button onClick={onClear}>
+          <FontAwesomeIcon icon={faTimes} />
+        </Button>
+      </Form>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {moviesResults && moviesResults.length > 0 && (
+            <Section title="영화 검색 결과">
+              {moviesResults.map((item) => (
+                <Poster
+                  key={item.id}
+                  content={item}
+                  id={item.id}
+                  title={
+                    item.original_title
+                      ? item.original_title
+                      : item.original_name
+                  }
+                  poster={item.poster_path}
+                />
+              ))}
+            </Section>
+          )}
+          {tvResults && tvResults.length > 0 && (
+            <Section title="TV 프로그램 검색 결과">
+              {tvResults &&
+                tvResults.map((item) => (
+                  <Poster
+                    key={item.id}
+                    content={item}
+                    id={item.id}
+                    title={
+                      item.original_title
+                        ? item.original_title
+                        : item.original_name
+                    }
+                    poster={item.poster_path}
+                  />
+                ))}
+            </Section>
+          )}
+        </>
+      )}
+    </Container>
+  );
+};
 
 export default SearchPresenter;
